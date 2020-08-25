a49fb3f401c6af3408aafcea3d73f76586e23906
Jorik van Rijn <jorik.vanrijn@gmail.com>
2020-08-25 12:53:05 +0200

    master complete

--- TrackCropper.py
+++ TrackCropper.py
@@ -1,26 +1,9 @@
 import ij.IJ as IJ
-# import ij.io.Opener as Opener
-# import ij.ImagePlus as ImagePlus
-# import ij.ImageStack as ImageStack
 import ij.WindowManager as WindowManager
-# import ij.measure.ResultsTable as ResultsTable
-# import ij.measure.Measurements as Measurements
-# import ij.plugin.ChannelSplitter as ChannelSplitter
-# import ij.plugin.HyperStackConverter as HyperStackConverter
-# import ij.plugin.ZProjector as ZProjector
-# import ij.plugin.RGBStackMerge as RGBStackMerge
-# import ij.plugin.StackCombiner as StackCombiner
-# import ij.plugin.MontageMaker as MontageMaker
-# import ij.plugin.StackCombiner as StackCombiner
-# import ij.plugin.Duplicator as Duplicator
-# import ij.plugin.Concatenator as Concatenator
-# import os
-
 from FijiTools2020.fileHandling import opencsv, getresults
 from FijiTools2020.impActions import croptracks, combinestacks
 
 
-# The main loop, call wanted functions.
 def main():
     # Get the wanted output directory and prepare subdirectories for output.
     outdir = IJ.getDirectory("output directory")
