7ad47ec371d840f7cb4272451da35110b1be6ddd
jrijn <jorik.vanrijn@gmail.com>
2021-02-05 11:27:02 +0100

    Calibration fixed for FlexoscopeFormatter

--- DICBacteriaExtractor.py
+++ DICBacteriaExtractor.py
@@ -1,23 +1,18 @@
-# This script is used for routine processing of two-channel stacks from
-# The flexoscope in the Sellin lab. The first (DIC) channel is filtered 
-# to remove non-mobile background using a medianfilter. The second (fluorescent 
-# bacteria) channel is filtered to remove all swimming bacteria using a
-# gliding median projection of every 3 frames. 
-#
 # Jorik van Rijn <jorik.vanrijn@gmail.com> - 2020
 
 import ij.IJ as IJ
 import ij.io.Opener as Opener
+import ij.ImagePlus as ImagePlus
 import ij.plugin.ChannelSplitter as ChannelSplitter
 import ij.plugin.RGBStackMerge as RGBStackMerge
-from FijiTools2020.impActions import subtractzproject, glidingprojection
+import ij.process.ImageProcessor as ImageProcessor
+from FijiTools2020.impActions import subtractzproject
 
 
 def main():
     # Open a .ome.tif image from the Flexoscope.
     impath = IJ.getFilePath("Choose .ome.tiff file")
     channels = Opener.openUsingBioFormats(impath)
-    cal = channels.getCalibration()
 
     # Show image
     # imp.show() # straight to channels object sames memory.
@@ -27,13 +22,20 @@ def main():
 
     # Process channel 1.
     # subtractzproject(imp, projectionMethod="Median")
-    channels[0] = subtractzproject(channels[0])
+    channels[1] = ImagePlus()
+    channels.append(ImagePlus())
+    channels[1] = subtractzproject(channels[0])
+    IJ.run(channels[0], "Enhance Contrast...", "saturated=0.3 normalize process_all use")
     IJ.run(channels[0], "8-bit", "") 
+    IJ.run(channels[1], "Square", "stack")
+    IJ.run(channels[1], "Enhance Contrast...", "saturated=0.3 normalize process_all use")
+    IJ.run(channels[1], "8-bit", "") 
+    
 
     # Process channel 2.
     # glidingprojection(imp, startframe=1, stopframe=None, glidingFlag=True, no_frames_per_integral=3, projectionmethod="Median")
-    channels[1] = glidingprojection(channels[1]) 
-    IJ.run(channels[1], "8-bit", "") 
+    # channels[1] = glidingprojection(channels[1]) 
+    # IJ.run(channels[1], "8-bit", "") 
 
     # [Optional] Process channel 3, 4, etc.
     # subtractzproject(channels[3], projectionMethod="Median")
@@ -42,9 +44,7 @@ def main():
 
     # Merge channels.
     merge = RGBStackMerge().mergeChannels(channels, True) # boolean keep
-    merge.setCalibration(cal)
     merge.show()
 
 
 main()
-
